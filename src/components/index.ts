8138cdc7c16b0c68e1a31dedaccd3ab6ab8f7343
Pooji <pooji@gmail.com>
2026-05-26 11:23:14 +0530

    Update src/components/index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,13 +1,15 @@
 // Export everything from the emulator components package so the frontend can import it
 export { BaseComponent } from './BaseComponent';
-export * from '../protocol-handlers/index.ts';
+export * from '../protocol-handlers/index';
 export { default as openhwLed } from './openhw-led';
 export { default as openhwArduinoUno } from './openhw-arduino-uno';
-export { default as openhwPico } from './openhw-pico/index.ts';
-export { default as openhwPicoW } from './openhw-pico-w/index.ts';
-export { default as openhwEsp32 } from './ESP32/index.ts';
+export { default as openhwPico } from './openhw-pico/index';
+export { default as openhwPicoW } from './openhw-pico-w/index';
+export { default as openhwEsp32 } from './ESP32/index';
 export { default as openhwResistor } from './openhw-resistor';
 export { default as openhwPushbutton } from './openhw-pushbutton';
+export { default as openhwPushbutton6mm } from './openhw-6mm-pushbutton';
+export { default as openhwStm32BluePill } from './openhw-stm32-blue-pill';
 export { default as openhwPowerSupply } from './openhw-power-supply';
 export { default as openhwNeopixelMatrix } from './openhw-neopixel-matrix';
 export { default as openhwBuzzer } from './openhw-buzzer';
