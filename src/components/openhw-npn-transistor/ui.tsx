9cb0646d342a4c8760193b315e757d2795cfc1e4
B Naga Krishna Manohar <krishnamanohar1179@gmail.com>
2026-06-10 16:49:08 +0530

    feat: add NPN transistor UI component with TO-92 styling

--- src/components/openhw-npn-transistor/ui.tsx
+++ src/components/openhw-npn-transistor/ui.tsx
@@ -5,7 +5,7 @@ export const BOUNDS = { x: 0, y: 0, w: 60, h: 90 };
 
 export const NPNTransistorUI = ({ state, attrs }: { state: any, attrs: any }) => {
     return (
-        <svg width="100%" height="100%" viewBox="0 0 30 45" xmlns="http://www.w3.org/2000/svg" style={{ overflow: 'visible', pointerEvents: 'none' }}>
+        <svg width={BOUNDS.w} height={BOUNDS.h} viewBox="0 0 30 45" xmlns="http://www.w3.org/2000/svg" style={{ overflow: 'visible', pointerEvents: 'none', display: 'block' }}>
             <defs>
                 <linearGradient id="legGradient" x1="0" y1="0" x2="1" y2="0">
                     <stop offset="0%" stopColor="#9E9E9E" />
